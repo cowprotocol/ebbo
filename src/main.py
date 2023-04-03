a99ef5041923247cc139a7dfd7cb9f66b86d5e77
Shubh Agarwal <mailtoshubh@gmail.com>
2023-04-03 16:11:16 -0400

    black fix

--- src/main.py
+++ src/main.py
@@ -4,11 +4,13 @@ Test Deployment file
 import logging
 import time
 
+
 def main() -> None:
-    """ Main Method"""
+    """Main Method"""
     while True:
         logging.warning("CoW Protocol!")
         time.sleep(10)
+
 
 if __name__ == "__main__":
     main()
