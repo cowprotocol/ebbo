9d54320cb4f63c099e245410e3de4f739b61353a
Shubh Agarwal <59472112+shubhagarwal03@users.noreply.github.com>
2023-04-03 16:41:22 -0400

    test deploy file (#12)

---
+++ src/main.py
@@ -0,0 +1,16 @@
+"""
+Test Deployment file
+"""
+import logging
+import time
+
+
+def main() -> None:
+    """Main Method"""
+    while True:
+        logging.warning("CoW Protocol!")
+        time.sleep(10)
+
+
+if __name__ == "__main__":
+    main()
