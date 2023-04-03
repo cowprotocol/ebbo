752b89becd47c08413022cf19a7982db7f9e80c8
Shubh Agarwal <mailtoshubh@gmail.com>
2023-04-03 16:09:45 -0400

    docstring

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
+"""
+Test Deployment file
+"""
 import logging
 import time
 
 def main() -> None:
+    """ Main Method"""
     while True:
         logging.warning("CoW Protocol!")
         time.sleep(10)
