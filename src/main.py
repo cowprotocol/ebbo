397e807eba5d13cd43dc3a290b5183c9c1cbbfd0
Haris Angelidakis <64154020+harisang@users.noreply.github.com>
2023-04-20 02:38:54 +0300

    Experiment with log levels (#21)
    Yet another attempt to clarify log levels...

--- src/main.py
+++ src/main.py
@@ -4,13 +4,15 @@ Test Deployment file
 import logging
 import time
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+# logging.basicConfig(level=logging.DEBUG)
 
 
 def main() -> None:
     """Main Method"""
     while True:
-        logging.error("EBBO running...")
+        logging.info("EBBO running...")
         time.sleep(10)
 
 
