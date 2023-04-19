6e424162efe5c8afda0021492d81c3fea36348c6
harisang <harisangelidakis@gmail.com>
2023-04-20 02:35:34 +0300

    fixed format

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ import time
 
 logging.basicConfig()
 logging.getLogger().setLevel(logging.DEBUG)
-#logging.basicConfig(level=logging.DEBUG)
+# logging.basicConfig(level=logging.DEBUG)
 
 
 def main() -> None:
