02b8d126cd132317a9723d7975ffd45e830c9c4e
Shubh Agarwal <mailtoshubh@gmail.com>
2023-04-20 10:39:42 -0400

    edit 5

--- src/main.py
+++ src/main.py
@@ -5,19 +5,11 @@ import logging
 import time
 
 
-def get_logger() -> logging.Logger:
-    """
-    logging
-    """
+def main() -> None:
+    """Main Method"""
     logging.basicConfig(format="%(levelname)s - %(message)s")
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
-    return logger
-
-
-def main() -> None:
-    """Main Method"""
-    logger = get_logger()
     while True:
         logger.warning("CoW Protocol!")
         time.sleep(10)
