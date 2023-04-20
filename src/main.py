c737bda19821eef1714520a78f58967ce6ea8218
harisang <harisangelidakis@gmail.com>
2023-04-21 00:01:16 +0300

    redirect to std err

--- src/main.py
+++ src/main.py
@@ -3,14 +3,13 @@ Test Deployment File
 """
 import logging
 import time
-import sys
 
 
 def main() -> None:
     """Main Method"""
     logging.basicConfig(format="%(levelname)s - %(message)s")
     logger = logging.getLogger()
-    logger.addHandler(logging.StreamHandler(sys.stdout))
+    logger.addHandler(logging.StreamHandler())
     logger.setLevel(logging.DEBUG)
     while True:
         logger.warning("CoW Protocol!")
