9a50e3e54a1c9ab1bb0277e21080dbed22574ff8
harisang <harisangelidakis@gmail.com>
2023-04-21 00:06:50 +0300

    add explicit mention to stderr

--- src/main.py
+++ src/main.py
@@ -3,13 +3,14 @@ Test Deployment File
 """
 import logging
 import time
+import sys
 
 
 def main() -> None:
     """Main Method"""
     logging.basicConfig(format="%(levelname)s - %(message)s")
     logger = logging.getLogger()
-    logger.addHandler(logging.StreamHandler())
+    logger.addHandler(logging.StreamHandler(sys.stderr))
     logger.setLevel(logging.DEBUG)
     while True:
         logger.warning("CoW Protocol!")
