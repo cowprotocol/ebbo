f0134a9dc8a24eae8d595e5cf2e21112168ff8bf
Shubh Agarwal <mailtoshubh@gmail.com>
2023-04-20 10:11:48 -0400

    logger edits 1

--- src/main.py
+++ src/main.py
@@ -1,16 +1,25 @@
 """
-Test Deployment file
+Test Deployment File
 """
 import logging
 import time
 
-logging.basicConfig(level=logging.DEBUG)
+
+def get_logger() -> logging.Logger:
+    """
+    get_logger() returns a logger object.
+    """
+    logging.basicConfig(format="%(levelname)s - %(message)s")
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    return logger
 
 
 def main() -> None:
     """Main Method"""
+    logger = get_logger()
     while True:
-        logging.info("Ebbo running...")
+        logger.warning("CoW Protocol!")
         time.sleep(10)
 
 
