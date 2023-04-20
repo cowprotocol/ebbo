dba7a411b92934f2fcf2103225e2820adf2a093e
Shubh Agarwal <59472112+shubhagarwal03@users.noreply.github.com>
2023-04-20 12:03:23 -0400

    Fix logging new (#23)
    Co-authored-by: sunce86 <dusan.stanivukovic@gmail.com>

--- src/main.py
+++ src/main.py
@@ -4,24 +4,12 @@ Test Deployment File
 import logging
 import time
 
-logging.basicConfig()
-logging.getLogger().setLevel(logging.DEBUG)
-# logging.basicConfig(level=logging.DEBUG)
-
-
-def get_logger() -> logging.Logger:
-    """
-    get_logger() returns a logger object.
-    """
-    logging.basicConfig(format="%(levelname)s - %(message)s")
-    logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
-    return logger
-
 
 def main() -> None:
     """Main Method"""
-    logger = get_logger()
+    logging.basicConfig(format="%(levelname)s - %(message)s")
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
     while True:
         logger.warning("CoW Protocol!")
         time.sleep(10)
