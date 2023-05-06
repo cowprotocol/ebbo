c5da1e2546e1c87bfb968865d0425744f6af51d0
harisang <harisangelidakis@gmail.com>
2023-05-07 00:13:29 +0300

    switch to relative economic viability test for alternative solutions

--- src/off_chain/cow_endpoint_surplus.py
+++ src/off_chain/cow_endpoint_surplus.py
@@ -191,10 +191,7 @@ class EBBOAnalysis:
                 surplus_deviation_dict = {}
                 soln_count = 0
                 for soln in competition_data["solutions"]:
-                    if (
-                        soln["objective"]["fees"] + 0.001 * pow(10, 18)
-                        < soln["objective"]["cost"]
-                    ):
+                    if soln["objective"]["fees"] < 0.9 * soln["objective"]["cost"]:
                         surplus_deviation_dict[soln_count] = 0.0, 0.0
                         soln_count += 1
                         continue
