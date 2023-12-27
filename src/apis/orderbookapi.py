112e35a1c7ab4a559b9a8a82a55c50f1a972f978
Felix Henneke <felix.henneke@protonmail.com>
2023-12-27 09:45:30 +0100

    Deactivate both cost coverage tests (#93)
    Since fee data is not available anymore, both tests do not produce reasonable results. Since we might want to use modified versions of the tests, they are just removed from the daemon for now.

--- src/apis/orderbookapi.py
+++ src/apis/orderbookapi.py
@@ -7,7 +7,6 @@ from typing import Any, Optional
 import json
 import requests
 from src.helper_functions import get_logger
-from src.models import Trade, OrderExecution
 from src.constants import (
     header,
     REQUEST_TIMEOUT,
@@ -58,69 +57,3 @@ class OrderbookAPI:
             )
             return None
         return solver_competition_data
-
-    def get_quote(self, trade: Trade) -> Optional[Trade]:
-        """
-        Given a trade, compute buy_amount, sell_amount, and fee_amount of the trade
-        as proposed by our quoting infrastructure.
-        """
-
-        if trade.data.is_sell_order:
-            kind = "sell"
-            limit_amount_name = "sellAmountBeforeFee"
-            executed_amount = trade.execution.sell_amount
-        else:
-            kind = "buy"
-            limit_amount_name = "buyAmountAfterFee"
-            executed_amount = trade.execution.buy_amount
-
-        request_dict = {
-            "sellToken": trade.data.sell_token,
-            "buyToken": trade.data.buy_token,
-            "receiver": "0x0000000000000000000000000000000000000000",
-            "appData": "0x0000000000000000000000000000000000000000000000000000000000000000",
-            "partiallyFillable": False,
-            "sellTokenBalance": "erc20",
-            "buyTokenBalance": "erc20",
-            "from": "0x0000000000000000000000000000000000000000",
-            "priceQuality": "optimal",
-            "signingScheme": "eip712",
-            "onchainOrder": False,
-            "kind": kind,
-            limit_amount_name: str(executed_amount),
-        }
-        prod_endpoint_url = f"{PROD_BASE_URL}quote"
-
-        try:
-            quote_response = requests.post(
-                prod_endpoint_url,
-                headers=header,
-                json=request_dict,
-                timeout=REQUEST_TIMEOUT,
-            )
-        except requests.RequestException as err:
-            self.logger.warning(
-                f"Fee quote failed. Request: {request_dict}, error: {err}"
-            )
-            return None
-
-        if quote_response.status_code != SUCCESS_CODE:
-            error_response_json = json.loads(quote_response.content)
-            self.logger.warning(
-                f"Error {error_response_json['errorType']},"
-                + f"{error_response_json['description']} while getting quote for trade {trade}"
-            )
-            return None
-
-        quote_json = json.loads(quote_response.text)
-        self.logger.debug("Quote received: %s", quote_json)
-
-        quote_buy_amount = int(quote_json["quote"]["buyAmount"])
-        quote_sell_amount = int(quote_json["quote"]["sellAmount"])
-        quote_fee_amount = int(quote_json["quote"]["feeAmount"])
-
-        quote_execution = OrderExecution(
-            quote_buy_amount, quote_sell_amount, quote_fee_amount
-        )
-
-        return Trade(trade.data, quote_execution)
