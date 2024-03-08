ddde9e42e2ad3235baf8e9b707516d3f0a5aeaa7
harisang <harisangelidakis@gmail.com>
2024-03-08 19:24:35 +0200

    remove empty line

--- src/monitoring_tests/uniform_directed_prices_test.py
+++ src/monitoring_tests/uniform_directed_prices_test.py
@@ -9,7 +9,6 @@ from src.constants import (
     UDP_SENSITIVITY_THRESHOLD,
 )
 
-
 class UniformDirectedPricesTest(BaseTest):
     """
     This test checks whether the Uniform Directed Prices constraint,
