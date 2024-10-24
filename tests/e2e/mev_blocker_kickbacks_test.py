30648706269f75bc5591de41ab4892a0a24130cd
harisang <harisangelidakis@gmail.com>
2024-10-24 15:15:43 +0300

    fix names in test

--- tests/e2e/mev_blocker_kickbacks_test.py
+++ tests/e2e/mev_blocker_kickbacks_test.py
@@ -1,5 +1,5 @@
 """
-Tests for large score test.
+Test for MEV Blocker kickbacks test.
 """
 
 import unittest
@@ -8,16 +8,16 @@ from src.monitoring_tests.mev_blocker_kickbacks_test import (
 )
 
 
-class TestHighScore(unittest.TestCase):
-    def test_high_score(self) -> None:
-        high_score_test = MEVBlockerRefundsMonitoringTest()
+class TestMevBlockerRefunds(unittest.TestCase):
+    def test_mev_blocker_refunds(self) -> None:
+        mev_blocker_refunds_test = MEVBlockerRefundsMonitoringTest()
         # large kickback tx
         tx_hash = "0xcbf4677177fb320b7e000ca95b31b5259648c75ebcfa9544014298ddfea94282"
-        self.assertTrue(high_score_test.run(tx_hash))
+        self.assertTrue(mev_blocker_refunds_test.run(tx_hash))
 
         # no kickback tx
         tx_hash = "0x3198bc18bc41ec3eb35cc382697d18917ebdaf03528e7dcc5270488d156037c8"
-        self.assertTrue(high_score_test.run(tx_hash))
+        self.assertTrue(mev_blocker_refunds_test.run(tx_hash))
 
 
 if __name__ == "__main__":
